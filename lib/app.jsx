857cce4ce44154437c4976dbaa1e62729c959bef
a-Name <bruno_moricet@protonmail.ch>
2015-11-17 15:27:12 +0100

    step 6

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,45 +1,49 @@
 'use strict';
 
-var React = require('react');
-
-var btnStyle = {
+var divStyle = {
   height:'100px',
-  width:'100px'
+  width:'100px',
 };
 
+var React = require('react');
 var Box = React.createClass({
+  getInitialState: function() {
+    return {value:this.props.initialValue};
+  },
+/*
+  componentWillMount: function(){
+    var _this = this;
+    this.timer = setInterval(function(){
+      var oldValue = _this.state.value;
+      var newValue = oldValue === 'X' ? 'O': 'X';
+      _this.setState({
+          value: newValue
+      });
+    }, 300);
+  },
+*/
+  handleClick: function() {
+    if(this.state.value === 'O'){
+      this.setState({value: 'X'});
+    }
+    if(this.state.value === 'X'){
+      this.setState({value: 'O'});
+    }
+  },
+
+componentWillUnmount: function(){
+  clearInterval(this.timer);
+},
+
   /**
    * Render a HTML button
    * @return {ReactElement}
    */
-  getInitialState: function() {
-    return {value : this.props.initialValue};
-  },
-
-  /*componentWillMount: function(){
-   	var oldThis = this;
-   	this.timer = setInterval(function(){
-   	var oldValue = oldThis.state.value;
-   	var newValue = oldValue === 'X' ? 'O': 'X';
-   	oldThis.setState({
-   	value: newValue
-   	});
-   	}, 300);
- 	},
- 	componentWillUnmount: function(){
- 	  clearInterval(this.timer);
- 	},*/
-
-  handleClick : function(){
-    var oldValue = oldThis.state.value;
-   	var newValue = oldValue === 'X' ? 'O': 'X';
-    this.setState({value = newValue});
-  }
-
   'render': function onRender () {
     return (
-      <button onClick={this.handleClick} style={btnStyle}>{this.state.value}</button>
+      <button style={divStyle} onClick={this.handleClick}>{this.state.value}</button>
     );
   }
 });
+
 React.render(<Box initialValue='X'/>, document.body);
